deb9322ea12d63d3e8542e1e609dc7b5c2491a6e
Anthony Mittaz <sync@me.com>
2024-01-04 16:46:55 +1100

    Setup lsp

---
+++ lua/dblechoc/lazy/lsp.lua
@@ -0,0 +1,97 @@
+return {
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"j-hui/fidget.nvim",
+	},
+	config = function()
+		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+		end
+
+		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		local default_setup = function(server_name)
+			require("lspconfig")[server_name].setup({
+				capabilities = lsp_capabilities,
+			})
+		end
+
+		require("fidget").setup()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"eslint",
+				"golangci_lint_ls",
+				"gopls",
+				"lua_ls",
+				"rust_analyzer",
+				"tailwindcss",
+				"tsserver",
+				"yamlls",
+			},
+			automatic_installation = true,
+			handlers = {
+				default_setup,
+
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup({
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+							},
+						},
+					})
+				end,
+			},
+		})
+
+		local cmp = require("cmp")
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			}, {
+				{ name = "buffer" },
+			}),
+		})
+
+		vim.diagnostic.config({
+			update_in_insert = true,
+			float = {
+				focusable = false,
+				border = "rounded",
+			},
+		})
+	end,
+}
