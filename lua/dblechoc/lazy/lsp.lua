96e8fb16eb923e0f1d8576d1c600f4479a31886a
Anthony Mittaz <sync@me.com>
2024-01-10 11:38:56 +1100

    Avoid going to a d.ts file if more than one result

--- lua/dblechoc/lazy/lsp.lua
+++ lua/dblechoc/lazy/lsp.lua
@@ -31,6 +31,23 @@ return {
 			require("cmp_nvim_lsp").default_capabilities()
 		)
 
+		local baseDefinitionHandler = vim.lsp.handlers["textDocument/definition"]
+
+		local filter = function(arr, fn)
+			if type(arr) ~= "table" then
+				return arr
+			end
+
+			local filtered = {}
+			for k, v in pairs(arr) do
+				if fn(v, k, arr) then
+					table.insert(filtered, v)
+				end
+			end
+
+			return filtered
+		end
+
 		require("fidget").setup()
 		require("mason").setup()
 		require("mason-tool-installer").setup({
@@ -56,6 +73,34 @@ return {
 				function(server_name)
 					require("lspconfig")[server_name].setup({
 						capabilities = capabilities,
+					})
+				end,
+
+				["tsserver"] = function()
+					local lspconfig = require("lspconfig")
+
+					local filterReactDTS = function(value)
+						if value.uri then
+							return string.match(value.uri, "%.d.ts") == nil
+						elseif value.targetUri then
+							return string.match(value.targetUri, "%.d.ts") == nil
+						end
+					end
+
+					local handlers = {
+						["textDocument/definition"] = function(err, result, method, ...)
+							if vim.tbl_islist(result) and #result > 1 then
+								local filtered_result = filter(result, filterReactDTS)
+								return baseDefinitionHandler(err, filtered_result, method, ...)
+							end
+
+							baseDefinitionHandler(err, result, method, ...)
+						end,
+					}
+
+					lspconfig.tsserver.setup({
+						capabilities = capabilities,
+						handlers = handlers,
 					})
 				end,
 
