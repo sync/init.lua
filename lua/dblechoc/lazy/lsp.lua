b9ed76fb045b243e16a81367467942f9a149c8b6
Anthony Mittaz <sync@me.com>
2025-05-16 09:58:44 +1000

    Update reference to mason and fixup lsp config since 0.11 upgrade

--- lua/dblechoc/lazy/lsp.lua
+++ lua/dblechoc/lazy/lsp.lua
@@ -1,7 +1,7 @@
 return {
-	"williamboman/mason-lspconfig.nvim",
+	"mason-org/mason-lspconfig.nvim",
 	dependencies = {
-		"williamboman/mason.nvim",
+		"mason-org/mason.nvim",
 		"neovim/nvim-lspconfig",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
@@ -16,42 +16,60 @@ return {
 		"onsails/lspkind.nvim",
 	},
 	config = function()
-		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-		end
-
-		local cmp = require("cmp")
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			require("cmp_nvim_lsp").default_capabilities()
-		)
-
-		local baseDefinitionHandler = vim.lsp.handlers["textDocument/definition"]
-
-		local filter = function(arr, fn)
-			if type(arr) ~= "table" then
-				return arr
-			end
-
-			local filtered = {}
-			for k, v in pairs(arr) do
-				if fn(v, k, arr) then
-					table.insert(filtered, v)
-				end
-			end
-
-			return filtered
-		end
-
-		local lspconfig = require("lspconfig")
-
 		require("fidget").setup()
+
+		vim.lsp.config("*", {
+			capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				require("cmp_nvim_lsp").default_capabilities()
+			),
+		})
+
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+					},
+					diagnostics = {
+						globals = {
+							"vim",
+							"require",
+						},
+					},
+				},
+			},
+		})
+
+		vim.lsp.config("ts_ls", {
+			init_options = {
+				preferences = {
+					includeInlayParameterNameHints = "none",
+					includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+					includeInlayFunctionParameterTypeHints = false,
+					includeInlayVariableTypeHints = false,
+					includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+					includeInlayPropertyDeclarationTypeHints = false,
+					includeInlayFunctionLikeReturnTypeHints = true,
+					includeInlayEnumMemberValueHints = true,
+				},
+			},
+			on_attach = function(client, bufnr)
+				vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+			end,
+		})
+
+		vim.lsp.config("eslint", {
+			settings = {
+				workingDirectories = { mode = "auto" },
+				experimental = {
+					useFlatConfig = false,
+				},
+			},
+		})
+
 		require("mason").setup()
 		require("mason-tool-installer").setup({
 			ensure_installed = {
@@ -66,118 +84,15 @@ return {
 			},
 			auto_update = false,
 		})
+
 		require("mason-lspconfig").setup({
-			automatic_installation = true,
-			handlers = {
-				function(server_name)
-					lspconfig[server_name].setup({
-						capabilities = capabilities,
-						-- on_attach = on_attach,
-					})
-				end,
-
-				["ts_ls"] = function()
-					local filterReactDTS = function(value)
-						if value.uri then
-							return string.match(value.uri, "%.d.ts") == nil
-						elseif value.targetUri then
-							return string.match(value.targetUri, "%.d.ts") == nil
-						end
-					end
-
-					local handlers = {
-						["textDocument/definition"] = function(err, result, method, ...)
-							if vim.tbl_islist(result) and #result > 1 then
-								local filtered_result = filter(result, filterReactDTS)
-								return baseDefinitionHandler(err, filtered_result, method, ...)
-							end
-
-							baseDefinitionHandler(err, result, method, ...)
-						end,
-						["textDocument/publishDiagnostics"] = function(_, result, ctx, config)
-							if result.diagnostics ~= nil then
-								local idx = 1
-								while idx <= #result.diagnostics do
-									local diagnostic = result.diagnostics[idx]
-									if diagnostic.code == 80001 then
-										table.remove(result.diagnostics, idx)
-									else
-										idx = idx + 1
-									end
-								end
-							end
-							vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
-						end,
-					}
-
-					lspconfig.ts_ls.setup({
-						capabilities = capabilities,
-						on_attach = function(client, bufnr)
-							vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-						end,
-						handlers = handlers,
-						init_options = {
-							preferences = {
-								includeInlayParameterNameHints = "none",
-								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-								includeInlayFunctionParameterTypeHints = false,
-								includeInlayVariableTypeHints = false,
-								includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-								includeInlayPropertyDeclarationTypeHints = false,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
-							},
-						},
-					})
-				end,
-				["eslint"] = function()
-					lspconfig.eslint.setup({
-						capabilities = capabilities,
-						settings = {
-							workingDirectories = { mode = "auto" },
-							experimental = {
-								useFlatConfig = false,
-							},
-						},
-						on_attach = function(client, bufnr)
-							vim.api.nvim_create_autocmd("BufWritePre", {
-								buffer = bufnr,
-								command = "EslintFixAll",
-							})
-						end,
-					})
-				end,
-				["lua_ls"] = function()
-					lspconfig.lua_ls.setup({
-						capabilities = capabilities,
-						settings = {
-							Lua = {
-								diagnostics = {
-									globals = { "vim" },
-								},
-							},
-						},
-					})
-				end,
-				["rust_analyzer"] = function()
-					lspconfig.rust_analyzer.setup({
-						capabilities = capabilities,
-						on_attach = function(client, bufnr)
-							vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-						end,
-						settings = {
-							["rust-analyzer"] = {
-								checkOnSave = {
-									command = "clippy",
-								},
-							},
-						},
-					})
-				end,
-			},
+			ensure_installed = {},
+			automatic_installation = false,
 		})
 
 		local lspkind = require("lspkind")
+		local cmp = require("cmp")
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 		cmp.setup({
 			formatting = {
@@ -229,6 +144,8 @@ return {
 			}),
 		})
 
+		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
 		vim.diagnostic.config({
 			virtual_lines = true,
 			update_in_insert = true,
@@ -238,11 +155,17 @@ return {
 			},
 			signs = {
 				active = true,
-				values = {
-					{ name = "DiagnosticSignError", text = signs.Error },
-					{ name = "DiagnosticSignWarn", text = signs.Warn },
-					{ name = "DiagnosticSignHint", text = signs.Hint },
-					{ name = "DiagnosticSignInfo", text = signs.Info },
+				text = {
+					[vim.diagnostic.severity.ERROR] = signs.Error,
+					[vim.diagnostic.severity.WARN] = signs.warm,
+					[vim.diagnostic.severity.INFO] = signs.info,
+					[vim.diagnostic.severity.HINT] = signs.hint,
+				},
+				texthl = {
+					[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+					[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+					[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+					[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
 				},
 			},
 		})
